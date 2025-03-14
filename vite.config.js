b4e015ebcb270a79ec9c5077d1b31429f434e78d
ubp <ubpkrkm@gmail.com>
2025-03-14 15:53:23 +0200

    start2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/goit-react-hw-011/', // Вказуємо базовий шлях для GitHub Pages
+});
